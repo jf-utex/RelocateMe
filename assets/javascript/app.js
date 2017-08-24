63ff17f235646308dbc4439a926a030d881de06a
Jeannie <jflaherty@utexas.edu>
2017-08-24 14:26:41 -0500

    added zillow image and test jquery api and ajax to js

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+
+
+// Jeannie working through ajax and API for Zillow
+// $.ajax({ url: queryURL, method: 'GET' })
+// .done(function(response) {
+
+
+//     $(".panel-body").on("click", ".buttons", function() {
+//         inputName = $(this).attr("data-name");
+//         queryURL = "https:www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1fyhgwn6qyz_8fetn&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA"
+        
+        
+        
+//         "https://api.giphy.com/v1/gifs/search?q=" + classics + "&limit=10&rating=pg&api_key=4f140638c6744217b071e2e6b0d01ab8";
+//         $(".gifs-display").empty();
+//         getGifs();
+//         console.log(this).attr("data-name")
